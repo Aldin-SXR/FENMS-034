de5614ee6236ff627968feae84972bd6568713ce
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-02-27 09:00:23 +0100

    Style update.

--- src/main/java/week1/DoubleNode.java
+++ src/main/java/week1/DoubleNode.java
@@ -4,10 +4,4 @@ public class DoubleNode<Data> {
     Data data;
     DoubleNode<Data> next;
     DoubleNode<Data> prev;
-
-    public DoubleNode() { }
-
-    public DoubleNode(Data data) {
-        this.data = data;
-    }
 }
--- src/main/java/week1/Node.java
+++ src/main/java/week1/Node.java
@@ -3,10 +3,4 @@ package week1;
 public class Node <Data>{
     Data data;
     Node<Data> next;
-
-    public Node() { }
-
-    public Node(Data data) {
-        this.data = data;
-    }
 }
