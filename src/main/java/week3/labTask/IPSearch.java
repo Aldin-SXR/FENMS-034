a8bb72e0e655a1ce0e1dcb03c8430c6cf33b08fe
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-03-11 16:11:38 +0100

    Removed unused line.

--- src/main/java/week3/labTask/IPSearch.java
+++ src/main/java/week3/labTask/IPSearch.java
@@ -1,8 +1,6 @@
 package week3.labTask;
 
 public class IPSearch {
-    public static int numSteps = 0;
-
     public static IPAddress search(IPAddress[] ipAddresses, String ipAddress) {
         // replace the method body with your code
         return null;
