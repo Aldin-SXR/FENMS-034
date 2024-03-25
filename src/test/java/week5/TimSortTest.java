c8152cb8295b68a54bde844cb9114e51e0c93bb8
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-03-25 17:08:50 +0100

    Added Week 5 tests.

--- src/test/java/week5/TimSortTest.java
+++ src/test/java/week5/TimSortTest.java
@@ -1,12 +1,13 @@
-package week4;
+package week5;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import week4.labTask.BinaryInsertionSort;
 import week4.labTask.LinkedList;
+import week5.labTask.TimSort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BinaryInsertionSortTest {
+public class TimSortTest {
 
     private static class Student implements Comparable<Student> {
         String name;
@@ -25,135 +26,131 @@ public class BinaryInsertionSortTest {
 
     @Test
     public void testIntegerSorting() {
-        LinkedList<Integer> ll = new LinkedList<>();
+        Integer[] array = {5, -1, 7, 19, 3, 12, 4};
 
-        ll.addToRear(5);
-        ll.addToRear(-1);
-        ll.addToRear(7);
-        ll.addToRear(19);
-        ll.addToRear(3);
-        ll.addToRear(12);
-        ll.addToRear(4);
+        TimSort.sort(array, 4);
 
-        BinaryInsertionSort.sort(ll);
-
-        assertEquals(-1, ll.get(0));
-        assertEquals(3, ll.get(1));
-        assertEquals(4, ll.get(2));
-        assertEquals(5, ll.get(3));
-        assertEquals(7, ll.get(4));
-        assertEquals(12, ll.get(5));
-        assertEquals(19, ll.get(6));
+        assertEquals(-1, array[0]);
+        assertEquals(3, array[1]);
+        assertEquals(4, array[2]);
+        assertEquals(5, array[3]);
+        assertEquals(7, array[4]);
+        assertEquals(12, array[5]);
+        assertEquals(19, array[6]);
     }
 
     @Test
     public void testDoubleSorting() {
-        LinkedList<Double> ll = new LinkedList<>();
+        Double[] array = {3.4, -11.2, 43.6, 22.5, 7.12, 5.24, 66.2};
 
-        ll.addToRear(3.4);
-        ll.addToRear(-11.2);
-        ll.addToRear(43.6);
-        ll.addToRear(22.5);
-        ll.addToRear(7.12);
-        ll.addToRear(5.24);
-        ll.addToRear(66.2);
+        TimSort.sort(array, 4);
 
-        BinaryInsertionSort.sort(ll);
-
-        assertEquals(-11.2, ll.get(0));
-        assertEquals(3.4, ll.get(1));
-        assertEquals(5.24, ll.get(2));
-        assertEquals(7.12, ll.get(3));
-        assertEquals(22.5, ll.get(4));
-        assertEquals(43.6, ll.get(5));
-        assertEquals(66.2, ll.get(6));
+        assertEquals(-11.2, array[0]);
+        assertEquals(3.4, array[1]);
+        assertEquals(5.24, array[2]);
+        assertEquals(7.12, array[3]);
+        assertEquals(22.5, array[4]);
+        assertEquals(43.6, array[5]);
+        assertEquals(66.2, array[6]);
     }
 
     @Test
     public void testCharacterSorting() {
-        LinkedList<Character> ll = new LinkedList<>();
+        Character[] array = {'y', 'm', 'a', 'c', 's', 'h', 'b'};
 
-        ll.addToRear('y');
-        ll.addToRear('m');
-        ll.addToRear('a');
-        ll.addToRear('c');
-        ll.addToRear('s');
-        ll.addToRear('h');
-        ll.addToRear('b');
+        TimSort.sort(array, 4);
 
-        BinaryInsertionSort.sort(ll);
-
-        assertEquals('a', ll.get(0));
-        assertEquals('b', ll.get(1));
-        assertEquals('c', ll.get(2));
-        assertEquals('h', ll.get(3));
-        assertEquals('m', ll.get(4));
-        assertEquals('s', ll.get(5));
-        assertEquals('y', ll.get(6));
+        assertEquals('a', array[0]);
+        assertEquals('b', array[1]);
+        assertEquals('c', array[2]);
+        assertEquals('h', array[3]);
+        assertEquals('m', array[4]);
+        assertEquals('s', array[5]);
+        assertEquals('y', array[6]);
     }
 
     @Test
     public void testStringSorting() {
-        LinkedList<String> ll = new LinkedList<>();
+        String[] array = {"qSiVYcEivp", "KHvpckFnVd", "RLbAwtkquH", "YmWxBwcHOZ", "NNAXXpPkpR", "hfRcldttwR", "lWtzsZCyPh"};
 
-        ll.addToRear("qSiVYcEivp");
-        ll.addToRear("KHvpckFnVd");
-        ll.addToRear("RLbAwtkquH");
-        ll.addToRear("YmWxBwcHOZ");
-        ll.addToRear("NNAXXpPkpR");
-        ll.addToRear("hfRcldttwR");
-        ll.addToRear("lWtzsZCyPh");
+        TimSort.sort(array, 4);
 
-        BinaryInsertionSort.sort(ll);
-
-        assertEquals("KHvpckFnVd", ll.get(0));
-        assertEquals("NNAXXpPkpR", ll.get(1));
-        assertEquals("RLbAwtkquH", ll.get(2));
-        assertEquals("YmWxBwcHOZ", ll.get(3));
-        assertEquals("hfRcldttwR", ll.get(4));
-        assertEquals("lWtzsZCyPh", ll.get(5));
-        assertEquals("qSiVYcEivp", ll.get(6));
+        assertEquals("KHvpckFnVd", array[0]);
+        assertEquals("NNAXXpPkpR", array[1]);
+        assertEquals("RLbAwtkquH", array[2]);
+        assertEquals("YmWxBwcHOZ", array[3]);
+        assertEquals("hfRcldttwR", array[4]);
+        assertEquals("lWtzsZCyPh", array[5]);
+        assertEquals("qSiVYcEivp", array[6]);
     }
 
     @Test
     public void testCustomObjectSorting() {
-        LinkedList<Student> ll = new LinkedList<>();
+        Student[] array = {
+                new Student("Student A", 6.3),
+                new Student("Student B", 9.93),
+                new Student("Student C", 5.76),
+                new Student("Student D", 8.21),
+                new Student("Student E", 7.74),
+                new Student("Student F", 10.0),
+                new Student("Student G", 9.2)
+        };
 
-        ll.addToRear(new Student("Student A", 6.3));
-        ll.addToRear(new Student("Student B", 9.93));
-        ll.addToRear(new Student("Student C", 5.76));
-        ll.addToRear(new Student("Student D", 8.21));
-        ll.addToRear(new Student("Student E", 7.74));
-        ll.addToRear(new Student("Student F", 10.0));
-        ll.addToRear(new Student("Student G", 9.2));
+        TimSort.sort(array, 4);
 
-        BinaryInsertionSort.sort(ll);
-
-        assertEquals(5.76, ll.get(0).gpa);
-        assertEquals(6.3, ll.get(1).gpa);
-        assertEquals(7.74, ll.get(2).gpa);
-        assertEquals(8.21, ll.get(3).gpa);
-        assertEquals(9.2, ll.get(4).gpa);
-        assertEquals(9.93, ll.get(5).gpa);
-        assertEquals(10.0, ll.get(6).gpa);
+        assertEquals(5.76, array[0].gpa);
+        assertEquals(6.3, array[1].gpa);
+        assertEquals(7.74, array[2].gpa);
+        assertEquals(8.21, array[3].gpa);
+        assertEquals(9.2, array[4].gpa);
+        assertEquals(9.93, array[5].gpa);
+        assertEquals(10.0, array[6].gpa);
     }
 
     @Test
-    public void testInsertionPoint() {
-        LinkedList<Integer> ll = new LinkedList<>();
+    public void testCalculateRunLength() {
+        int threshold = 8;
+        assertEquals(5, TimSort.calculateRunLength(19, threshold));
+        assertEquals(9, TimSort.calculateRunLength(17, threshold));
+        assertEquals(6, TimSort.calculateRunLength(21, threshold));
+        assertEquals(7, TimSort.calculateRunLength(7, threshold));
+    }
 
-        ll.addToRear(-2);
-        ll.addToRear(3);
-        ll.addToRear(7);
-        ll.addToRear(12);
-        ll.addToRear(6);
-        ll.addToRear(19);
-        ll.addToRear(4);
+    @Test
+    public void testPartialInsertionSort() {
+        Integer[] array = {4, -2, 7, 19, 3, 12, 9, 21, -5, 17, 8};
 
-        assertEquals(0, BinaryInsertionSort.findInsertionPoint(ll, 3, -5));
-        assertEquals(1, BinaryInsertionSort.findInsertionPoint(ll, 3, 2));
-        assertEquals(2, BinaryInsertionSort.findInsertionPoint(ll, 3, 5));
-        assertEquals(3, BinaryInsertionSort.findInsertionPoint(ll, 3, 9));
-        assertEquals(4, BinaryInsertionSort.findInsertionPoint(ll, 3, 15));
+        TimSort.insertionSort(array, 0, 3);
+        TimSort.insertionSort(array, 6, 8);
+        TimSort.insertionSort(array, 9, 10);
+
+        assertEquals(-2, array[0]);
+        assertEquals(4, array[1]);
+        assertEquals(7, array[2]);
+        assertEquals(19, array[3]);
+        assertEquals(3, array[4]);
+        assertEquals(12, array[5]);
+        assertEquals(-5, array[6]);
+        assertEquals(9, array[7]);
+        assertEquals(21, array[8]);
+        assertEquals(8, array[9]);
+        assertEquals(17, array[10]);
+    }
+
+    @Test
+    public void testMerge() {
+        Integer[] array = {-2, 4, 7, 19, 3, 9, 12, 21, -5, 8, 17};
+        Integer[] aux = new Integer[array.length];
+
+        TimSort.merge(array, aux, 0, 3, 7);
+
+        assertEquals(-2, array[0]);
+        assertEquals(3, array[1]);
+        assertEquals(4, array[2]);
+        assertEquals(7, array[3]);
+        assertEquals(9, array[4]);
+        assertEquals(12, array[5]);
+        assertEquals(19, array[6]);
+        assertEquals(21, array[7]);
     }
 }
